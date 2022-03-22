a9780e8774d91c7ca682d651774fb87a3bc5d017
Derick Canales <derickcanales97@gmail.com>
2022-03-22 11:03:16 -0600

    confuguraciones locales

--- gestion_riesgos/settings/local.py
+++ gestion_riesgos/settings/local.py
@@ -8,9 +8,9 @@ ALLOWED_HOSTS = []
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'gestionriesgos',
+    #     'NAME': '*********',
     #     'USER': 'derick',
-    #     'PASSWORD': 'djcz',
+    #     'PASSWORD': '****',
     #     'HOST': 'localhost',
     #     'PORT': '5432',
     # }
