e63ad9f13c4fe34fdd01cb586e9028213d5f26de
Derick Canales <derickcanales97@gmail.com>
2022-03-28 12:09:40 -0600

    serializadores para campos foraneos

--- apps/riesgos/serializer.py
+++ apps/riesgos/serializer.py
@@ -1,9 +1,40 @@
-from .models import Riesgos
+from .models import Categorias, FuenteRiesgos, MagnitudImpacto, Propietario, Riesgos, Servicios
 from rest_framework import serializers
+
+# class ServiciosSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Servicios
+#         fields = ('nombre','tipos_servicios')
+
+class PropietarioSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Propietario
+        fields = ['nombre']
+
+class CategoriasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categorias
+        fields = ['nombre']
+
+class FuenteRiesgosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FuenteRiesgos
+        fields = ['nombre']
+
+class MagnitudImpactoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MagnitudImpacto
+        fields = ['descripcion']
 
 class RiesgoSerializer(serializers.ModelSerializer):
     impacto_display = serializers.CharField(source = "get_impacto_display")
     probabilidad_display = serializers.CharField(source = "get_probabilidad_ocurrencia_display")
+    propietario = PropietarioSerializer()
+    categorias = CategoriasSerializer()
+    fuente_riesgo = FuenteRiesgosSerializer()
+    # riesgos_servicios = ServiciosSerializer()
+    magnitud_impacto = MagnitudImpactoSerializer()
 
     class Meta:
         model = Riesgos
